c7d43c48008e2f9e2fe6f3eb290fc44d8489dcb4
Bruno Krebs <bruno.krebs@auth0.com>
2018-06-21 11:05:00 -0300

    ### Defining a Nest.js Middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AuthenticationMiddleware } from './common/authentication.middleware';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const auth = new AuthenticationMiddleware().resolve();
+  server.post('/items', auth);
+  server.post('/shopping-cart', auth);
+
+  const app = await NestFactory.create(AppModule, server);
   await app.listen(3000);
 }
+
 bootstrap();
